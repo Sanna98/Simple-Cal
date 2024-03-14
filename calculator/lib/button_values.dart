2d4c7cc0e9ff23c1108e28941de8a9688af7c42e
sanna98 <samithaherath027@gmail.com>
2024-03-14 10:25:27 +0530

    Buttons initiating

--- calculator/lib/button_values.dart
+++ calculator/lib/button_values.dart
@@ -1,9 +1,9 @@
-class btn {
+class Btn {
   static const String del = "D";
   static const String clr = "C";
   static const String per = "%";
   static const String multiply = "x";
-  static const String divide = "%";
+  static const String divide = "/";
   static const String add = "+";
   static const String subtract = "-";
   static const String calculate = "=";
@@ -20,3 +20,24 @@ class btn {
   static const String n8 = "8";
   static const String n9 = "9";
 }
+
+static const List<String> buttonValues = [
+  Btn.del,
+  Btn.clr,
+  Btn.per,
+  Btn.multiply,
+  Btn.n7,
+  Btn.n8,
+  Btn.n9,
+  Btn.divide,
+  Btn.n4,
+  Btn.n5,
+  Btn.n6,
+  Btn.subtract,
+  Btn.n1,
+  Btn.n2,
+  Btn.n3,
+  Btn.add,
+  Btn.n0,
+  Btn.dot,
+];
